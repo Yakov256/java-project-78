874c351d6ba716235d4478036dfdef852a548e51
yakov256 <yakov256@yandex.ru>
2023-12-15 09:04:42 +0300

    code review changes: removed unnecessary data

--- app/src/main/java/hexlet/code/Validator.java
+++ app/src/main/java/hexlet/code/Validator.java
@@ -1,29 +1,21 @@
 package hexlet.code;
 
-import hexlet.code.schemas.BaseSchema;
 import hexlet.code.schemas.MapSchema;
 import hexlet.code.schemas.NumberSchema;
 import hexlet.code.schemas.StringSchema;
 
 public final class Validator {
-    private BaseSchema schema;
 
     public StringSchema string() {
-        StringSchema strSchema = new StringSchema();
-        schema = strSchema;
-        return strSchema;
+        return new StringSchema();
     }
 
     public NumberSchema number() {
-        NumberSchema numberSchema = new NumberSchema();
-        schema = numberSchema;
-        return numberSchema;
+        return new NumberSchema();
     }
 
     public MapSchema map() {
-        MapSchema mapSchema = new MapSchema();
-        schema = mapSchema;
-        return mapSchema;
+        return new MapSchema();
     }
 
 }
